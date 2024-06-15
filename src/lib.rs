f88efcc692b8317bf5e0cf6e568526491b388858
A4-Tacks <wdsjxhno1001@163.com>
2024-06-15 21:11:05 +0800

    init repo

---
+++ src/lib.rs
@@ -0,0 +1,331 @@
+use std::{borrow::Cow, cell::Cell, collections::HashMap, fmt, io};
+
+pub mod parser;
+
+macro_rules! fa {
+    (;$($t:tt)*) => {
+        ::std::format_args!("{}\n", ::std::format_args!($($t)*))
+    };
+    ($($t:tt)*) => {
+        ::std::format_args!($($t)*)
+    };
+}
+macro_rules! assert_match {
+    ($e:expr, $pat:pat $(if $cond:expr)? $(,)?) => {
+        match $e {
+            $pat $(if $cond)? => {},
+            ref val => {
+                panic!(
+                    "assert matches ({}) failed!\n left: {:?}\nright: {}",
+                    ::core::stringify!($e),
+                    val,
+                    ::core::stringify!($pat $(if $cond)?),
+                )
+            },
+        }
+    };
+    ($e:expr, $pat:pat $(if $cond:expr)?, $($f:tt)*) => {
+        match $e {
+            $pat $(if $cond)? => {},
+            ref val => {
+                panic!(
+                    "assert matches ({}) failed, `{}`\n left: {:?}\nright: {}",
+                    ::core::stringify!($e),
+                    ::core::format_args!($($f)*),
+                    val,
+                    ::core::stringify!($pat $(if $cond)?),
+                )
+            },
+        }
+    };
+}
+
+#[derive(Debug)]
+pub enum Error {
+    UndefineRef(String),
+    RepeatDefineName(String),
+    IOError(io::Error),
+}
+impl From<io::Error> for Error {
+    fn from(e: io::Error) -> Self {
+        Self::IOError(e)
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Expr<'a> {
+    /// regexp or string literal, and group count.
+    /// e.g /ab/ "abi"
+    Literal(Cow<'a, str>, u32),
+    /// e.g keywordsToRegex("ab cd", "ef gh")
+    KwdsToRegex(Vec<&'a str>),
+    /// rule reference, e.g @foo
+    Ref(&'a str),
+    /// Include extern regexp, and group count
+    /// e.g `&foo` `&foo(2)`
+    Include(Cow<'a, str>, u32),
+}
+impl<'a> fmt::Display for Expr<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Expr::Literal(lit, _) => f.write_str(&*lit),
+            Expr::KwdsToRegex(kwds) => {
+                write!(f, "keywordsToRegex(")?;
+                if let Some(first) = kwds.first() {
+                    write!(f, "{first}")?;
+                    for &kwd in kwds[1..].iter() {
+                        write!(f, " {kwd}")?;
+                    }
+                }
+                write!(f, ")")
+            },
+            Expr::Ref(name) => write!(f, "include(\"{name}\")"),
+            Expr::Include(name, _) => write!(f, "include({name})"),
+        }
+    }
+}
+impl Expr<'_> {
+    /// 统计组数, 如果是未确定的通过给定的函数获取组数
+    pub fn get_group_count<F>(&self, f: F) -> Result<u32>
+    where F: FnOnce(&str) -> Option<u32>,
+    {
+        Ok(match self {
+            | &Expr::Include(_, c)
+            | &Expr::Literal(_, c) => c,
+            | &Expr::KwdsToRegex(_) => 0,
+            | &Expr::Ref(name) => f(name)
+                .ok_or_else(|| Error::UndefineRef(name.into()))?,
+        })
+    }
+
+    pub fn build_colors<'a, F, C>(
+        &self,
+        octx: &mut OutputContext<'_, F>,
+        ctx: &BuildContext<'_>,
+        mut color: C,
+    ) -> Result<()>
+    where F: FnMut(std::fmt::Arguments<'_>) -> io::Result<()>,
+          C: Iterator<Item = Option<&'a str>>,
+    {
+        Ok(match self {
+            | &Expr::KwdsToRegex(_) => (),
+            | &Expr::Ref(name) => {
+                let rule = ctx.rule_map.get(name)
+                    .ok_or_else(|| Error::UndefineRef(name.into()))?;
+                rule.build_colors(octx, ctx)?
+            },
+            | &Expr::Literal(_, count)
+            | &Expr::Include(_, count) => {
+                let cur_color = ctx.current_color.get();
+
+                for i in 0..count {
+                    let id = i + cur_color;
+                    if let Some(Some(color)) = color.next() {
+                        octx.newline()?;
+                        octx.output(fa!("{id}: {color}"))?;
+                    }
+                }
+
+                ctx.current_color.set(cur_color + count);
+            },
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct Rule<'a> {
+    name: &'a str,
+    exprs: Vec<Expr<'a>>,
+    colors: Vec<Option<&'a str>>,
+    group_count: Option<u32>,
+    hidden: bool,
+    attrs: Vec<(&'a str, &'a str)>,
+}
+impl<'a> Rule<'a> {
+    pub fn update_group_count<F>(&mut self, mut f: F) -> Result<()>
+    where F: FnMut(&str) -> Option<u32>
+    {
+        if self.group_count.is_none() {
+            self.group_count = self.exprs.iter()
+                .try_fold(0, |acc, exp| {
+                    let count = exp.get_group_count(&mut f)?;
+                    Ok::<_, Error>(acc + count)
+                })?.into();
+        }
+        Ok(())
+    }
+
+    pub fn build_colors<F>(
+        &self,
+        octx: &mut OutputContext<'_, F>,
+        ctx: &BuildContext<'_>,
+    ) -> Result<()>
+    where F: FnMut(std::fmt::Arguments<'_>) -> io::Result<()>,
+    {
+        assert_match!(self.colors[..], [] | [None, ..]);
+        assert_ne!(self.exprs.len(), 0);
+
+        let mut color = self.colors
+            .iter()
+            .skip(1)
+            .map(Option::as_deref);
+
+        for expr in &self.exprs {
+            expr.build_colors(octx, ctx, color.by_ref())?;
+        }
+
+        Ok(())
+    }
+
+    pub fn build<F>(
+        &self,
+        ctx: &BuildContext<'_>,
+        octx: &mut OutputContext<'_, F>,
+    ) -> Result<()>
+    where F: FnMut(std::fmt::Arguments<'_>) -> io::Result<()>,
+    {
+        octx.output(fa!("\"{}\": ", self.name))?;
+        if self.hidden {
+            for expr in self.exprs.iter().take(self.exprs.len() - 1) {
+                octx.output(fa!("{expr} + "))?;
+            }
+            octx.outputln(fa!("{}", self.exprs.last().unwrap()))?;
+        } else {
+            octx.with_block(|octx| {
+                octx.output(fa!("match: "))?;
+                for expr in self.exprs.iter().take(self.exprs.len() - 1) {
+                    octx.output(fa!("{expr} + "))?;
+                }
+                octx.output(fa!("{}", self.exprs.last().unwrap()))?;
+
+                for &(attr, val) in &self.attrs {
+                    octx.newline()?;
+                    octx.output(fa!("{attr}: {val}"))?;
+                }
+
+                ctx.current_color.set(1);
+
+                self.build_colors(octx, ctx)?;
+                Result::Ok(())
+            })??;
+        }
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub struct BuildContext<'a> {
+    current_color: Cell<u32>,
+    rule_map: HashMap<String, Rule<'a>>,
+}
+impl Default for BuildContext<'static> {
+    fn default() -> Self {
+        Self {
+            current_color: 0.into(),
+            rule_map: HashMap::new(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct OutputContext<'a, F = fn(fmt::Arguments<'_>) -> io::Result<()>> {
+    pub newline_str: &'a str,
+    pub indent_str: &'a str,
+    pub indent_level: u32,
+    output: F,
+}
+
+impl<'a, F> OutputContext<'_, F>
+where F: FnMut(fmt::Arguments<'_>) -> io::Result<()>,
+{
+    pub fn new(output: F) -> Self {
+        Self {
+            newline_str: "\n",
+            indent_str: "    ",
+            indent_level: 0,
+            output,
+        }
+    }
+
+    pub fn newline(&mut self) -> io::Result<()> {
+        (self.output)(fa!("{}", self.newline_str))?;
+        self.indent()
+    }
+
+    pub fn output(&mut self, args: fmt::Arguments<'_>) -> io::Result<()> {
+        (self.output)(args)
+    }
+
+    pub fn outputln(&mut self, args: fmt::Arguments<'_>) -> io::Result<()> {
+        (self.output)(args)?;
+        self.newline()
+    }
+
+    pub fn indent(&mut self) -> io::Result<()> {
+        for _ in 0..self.indent_level {
+            (self.output)(fa!("{}", self.indent_str))?;
+        }
+        Ok(())
+    }
+
+    pub fn with_indent<F1, R>(&mut self, f: F1) -> R
+    where F1: FnOnce(&mut Self) -> R,
+    {
+        self.indent_level += 1;
+        let res = f(self);
+        self.indent_level -= 1;
+        res
+    }
+
+    pub fn with_block<F1, R>(&mut self, f: F1) -> io::Result<R>
+    where F1: FnOnce(&mut Self) -> R,
+    {
+        self.indent_level += 1;
+        (self.output)(fa!("{{"))?;
+        self.newline()?;
+
+        let res = f(self);
+
+        self.indent_level -= 1;
+        self.newline()?;
+        (self.output)(fa!("}}"))?;
+        self.newline()?;
+
+        Ok(res)
+    }
+}
+impl Default for OutputContext<'static> {
+    fn default() -> Self {
+        Self::new(|args| {
+            print!("{args}");
+            Ok(())
+        })
+    }
+}
+
+pub fn build<'a, I, F>(
+    rules: I,
+    octx: &mut OutputContext<'_, F>,
+    ctx: &mut BuildContext<'a>,
+) -> Result<()>
+where I: IntoIterator<Item = Rule<'a>>,
+      F: FnMut(std::fmt::Arguments<'_>) -> io::Result<()>,
+{
+    for mut rule in rules {
+        rule.update_group_count(|name| {
+            ctx.rule_map.get(name)
+                .map(|rule| rule.group_count.unwrap())
+        })?;
+
+        rule.build(ctx, octx)?;
+
+        if let Some(rule) = ctx.rule_map
+            .insert((*rule.name).into(), rule)
+        {
+            return Err(Error::RepeatDefineName((*rule.name).into()));
+        }
+    }
+    Ok(())
+}
---
+++ src/parser.rs
@@ -0,0 +1,319 @@
+pub use parser::*;
+
+use crate::{Rule, Expr};
+
+peg::parser!(grammar parser() for str {
+    rule newline()
+        = "\r"? "\n"
+
+    rule comment()
+        = "//" !"!" [^'\r' | '\n']*
+
+    rule _()
+        = quiet!{
+            (
+                [' ' | '\t']+
+                / comment()? newline()
+            )* (comment()? ![_])?
+        }
+
+    pub rule ident() -> &'input str
+        = quiet!{$(
+            !['0'..='9' | '-']
+            ['a'..='z' | 'A'..='Z' | '0'..='9' | '_' | '-']+
+        )}
+        / expected!("ident")
+
+    pub rule string() -> &'input str
+        = quiet!{
+            $("\"" (
+                "\\" [^'\r' | '\n']
+                / [^'"' | '\r' | '\n']
+            )* "\"")
+        }
+        / expected!("string")
+
+    pub rule regex() -> &'input str
+        = quiet!{
+            $("/" ("\\/" / [^'/' | '\r' | '\n'])+ "/")
+        }
+        / expected!("regex")
+
+    pub rule unum() -> u32
+        = s:quiet!{$("0" / !"0" ['0'..='9']+)}
+        {?
+            s.parse().map_err(|_| {
+                "invalid number"
+            })
+        }
+        / expected!("number")
+
+    rule color() -> (u32, &'input str)
+        = "$" n:unum() _ ":" _ name:string()
+        { (n, name) }
+
+    pub rule colors() -> Vec<Option<&'input str>>
+        = colors:color() ** _
+        {
+            let mut res = Vec::new();
+            for (id, color) in colors {
+                let id = id as usize;
+                if res.len() <= id { res.resize(id+1, None) }
+                res[id] = color.into();
+            }
+            res
+        }
+
+    rule attr_val() -> &'input str
+        = string()
+        / $("true")
+        / $("false")
+
+    rule attr() -> (&'input str, &'input str)
+        = "$" name:ident() _ ":" _ val:attr_val()
+        { (name, val) }
+
+    pub rule attrs() -> Vec<(&'input str, &'input str)>
+        = attrs:attr() ** _
+        {
+            attrs
+        }
+
+    rule group_count_regex_ig()
+        = "\\" ("\\"? "/" / [^ '/'])
+        / "(?" !("<" ['a'..='z' | 'A'..='Z'])
+        / [^'(' | '/' | '\\']
+
+    rule group_count_str_ig()
+        = "\\\\" [^ '"']
+        / "\\" [_]
+        / "(?" !("<" ['a'..='z' | 'A'..='Z'])
+        / [^'(' | '"' | '\\']
+
+    /// group count, contains `()` and `(?<name>)`
+    pub rule group_count() -> u32
+        = "/" group_count_regex_ig()*
+            s:("(" (&[^ '?'] / "?<" ['a'..='z' | 'A'..='Z']) group_count_regex_ig()*)*
+            "/"
+            { s.len() as u32 }
+        / "\"" group_count_str_ig()*
+            s:("(" (&[^ '?'] / "?<" ['a'..='z' | 'A'..='Z']) group_count_str_ig()*)*
+            "\""
+            { s.len() as u32 }
+
+    pub rule expr() -> Expr<'input>
+        = s:(regex() / string()) { Expr::Literal(s.into(), group_count(s).unwrap()) }
+        / "keywordsToRegex" "(" _ s:string() ++ (_ ("," _)?) _ ("," _)? ")"
+            {
+                Expr::KwdsToRegex(s)
+            }
+        / "@" i:ident() { Expr::Ref(i) }
+        / "&"
+            n:( i:ident() { format!("\"{i}\"").into() }
+              / s:string() { s.into() })
+            c:("(" n:unum() ")" { n })? { Expr::Include(n, c.unwrap_or(0)) }
+
+    pub rule mt_rule() -> Rule<'input>
+        = name:ident()
+        _ vars:(
+            ":="
+            _ exprs:expr() ++ (_ ("+" _)?)
+            _ attrs:attrs()
+            _ colors:colors()
+            { (false, exprs, attrs, colors) }
+          / "="
+            _ exprs:expr() ++ (_ ("+" _)?)
+            _ colors:colors()
+            { (true, exprs, vec![], colors) }
+        )
+        {
+            let (hidden, mut exprs, attrs, mut colors) = vars;
+
+            if let Some(first) = colors.first_mut()
+                .map(|color| color.take())
+                .flatten()
+            {
+                colors.insert(1, first.into());
+                let [b, e] = ["/(/", "/)/"];
+                match &mut exprs[..] {
+                    [Expr::Literal(s, n), ..] => {
+                        s.to_mut().insert(1, '(');
+                        *n += 1;
+                    },
+                    _ => exprs.insert(0, Expr::Literal("/(/".into(), 1)),
+                }
+                match &mut exprs[..] {
+                    [.., Expr::Literal(s, _)] => {
+                        let tail = s.to_mut().pop().unwrap();
+                        s.to_mut().push(')');
+                        s.to_mut().push(tail);
+                    },
+                    _ => exprs.push(Expr::Literal("/)/".into(), 0)),
+                }
+            }
+            Rule {
+                name,
+                exprs,
+                colors,
+                group_count: None,
+                hidden,
+                attrs,
+            }
+        }
+
+    pub rule rule_list() -> Vec<Rule<'input>>
+        = _ s:mt_rule() ++ _ _ { s }
+
+    pub rule script() -> (&'input str, Vec<Rule<'input>>, &'input str)
+        =
+        begin:$(
+            (
+                [' ' | '\t']*
+                !"//!includeBegin"
+                [^'\r' | '\n']*
+                newline()
+            )*
+            [' ' | '\t']*
+        )
+        "//!includeBegin" newline()
+        rules:rule_list()
+        [' ' | '\t']* "//!includeEnd" newline()
+        end:$([_]*)
+        {
+            (begin, rules, end)
+        }
+});
+
+#[cfg(test)]
+mod tests {
+    use crate::{build, BuildContext, OutputContext};
+
+    use super::*;
+
+    #[test]
+    fn parse_test() {
+        let src = r#"
+        foo = /(abc)/ + "def" + /./
+            $1: "red" // abc
+
+        bar := @foo + /;|\// + @foo // ...
+        "#;
+        println!("{src}");
+
+        let rules = parser::rule_list(src)
+            .unwrap_or_else(|e| {
+                eprintln!("line {} column {}", e.location.line, e.location.column);
+                eprintln!("expected {}", e.expected);
+                panic!()
+            });
+        dbg!(&rules);
+        let mut ctx = BuildContext::default();
+        let mut octx = OutputContext::default();
+        println!("-- output --");
+        build(rules, &mut octx, &mut ctx).unwrap();
+        println!("-- finished --");
+    }
+
+    #[test]
+    fn script_test() {
+        let src = r#"
+        some text of begin
+        //!includeBegin
+        foo = /(abc)/ + "def" + &extern(2)
+            $1: "red" // abc
+
+        bar := @foo + /;|\// + @foo // ...
+        //!includeEnd
+        some text of end
+        "#;
+        println!("{src}");
+
+        dbg!(script(src).unwrap());
+    }
+
+    #[test]
+    fn group_count_test() {
+        let datas = [
+            (r#"//"#, 0),
+            (r#"/m/"#, 0),
+            (r#"/mmmmmm/"#, 0),
+            (r#"/(?:m)/"#, 0),
+            (r#"/m(?:m)/"#, 0),
+            (r#"/m(?:m)m/"#, 0),
+            (r#"/()/"#, 1),
+            (r#"/m()/"#, 1),
+            (r#"/()m/"#, 1),
+            (r#"/m()m/"#, 1),
+            (r#"/m(m)m/"#, 1),
+            (r#"/m(m)m/"#, 1),
+            (r#"/m(?:m)m/"#, 0),
+            (r#"/m(?<=m)m/"#, 0),
+            (r#"/m\(m)m/"#, 0),
+            (r#"/m\\(m)m/"#, 1),
+            (r#"/m\m(m)m/"#, 1),
+            (r#"/m\\/(m)m/"#, 1),
+            (r#"/m\\\\/(m)m/"#, 1),
+            (r#"/m(?<a>)m/"#, 1),
+            (r#"/m\(?<a>)m/"#, 0),
+            (r#"/m\\(?<a>)m/"#, 1),
+            (r#"/m\\(?=a)m/"#, 0),
+            (r#"/m(?=a)m/"#, 0),
+            (r#"/(?=a)/"#, 0),
+            (r#"/(=a)/"#, 1),
+            (r#"/(a)/"#, 1),
+            (r#"/()/"#, 1),
+            (r#"/()()/"#, 2),
+            (r#"/m()()/"#, 2),
+            (r#"/m()m()/"#, 2),
+            (r#"/m()()m/"#, 2),
+            (r#"/(())/"#, 2),
+            (r#"/((()))/"#, 3),
+            (r#"/(()())/"#, 3),
+            (r#"/m(())/"#, 2),
+            (r#"/(m())/"#, 2),
+            (r#"/((m))/"#, 2),
+            (r#"/m(m())/"#, 2),
+            (r#"/((?:))/"#, 1),
+            (r#"/(?:())/"#, 1),
+            (r#"/((?=))/"#, 1),
+            (r#"/((?<=))/"#, 1),
+            (r#"/((?<x>))/"#, 2),
+            (r#"/(?<m>(?<x>))/"#, 2),
+
+            (r#""""#, 0),
+            (r#""m""#, 0),
+            (r#""()m""#, 1),
+            (r#""m()""#, 1),
+            (r#""(m)""#, 1),
+            (r#""m(m)""#, 1),
+            (r#""(m)m""#, 1),
+            (r#""m()m""#, 1),
+            (r#""m(m)m""#, 1),
+            (r#""m(())m""#, 2),
+            (r#""m(()())m""#, 3),
+            (r#""m((()))m""#, 3),
+            (r#""m((\\()))m""#, 2),
+            (r#""m(\\(\\()))m""#, 1),
+            (r#""m(\(\()))m""#, 1),
+            (r#""(?<m>(?<x>))""#, 2),
+            (r#""\\(?<m>(?<x>))""#, 1),
+            (r#""(?<m>\\(?<x>))""#, 1),
+        ];
+
+        for (src, count) in datas {
+            let c = match parser::group_count(src) {
+                Ok(c) => c,
+                Err(e) => {
+                    eprintln!("src: {src}");
+                    eprintln!("err: {},{}\nexpected: {}",
+                        e.location.line,
+                        e.location.column,
+                        e.expected,
+                    );
+                    panic!()
+                }
+            };
+            assert_eq!(c, count, "src: {src}");
+        }
+    }
+}
